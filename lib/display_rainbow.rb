23c796719def81eb03cc0afdb91d34ca2ea9020e
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-11 16:48:10 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,12 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
-  rainbow = ""
-  colors.each do |color|
-    rainbow += "#{color[0].upcase}: #{color}, "
-  end
+  # rainbow = ""
+  # colors.each do |color|
+  #   rainbow += "#{color[0].upcase}: #{color}, "
+  # end
   
-  puts rainbow.chomp(", ")
+  # puts rainbow.chomp(", ")
   
   puts "#{colors[0][0]}: #{colors[0]}, #{colors[1][0]}: #{colors[1]}, #{colors[2][0]}: #{colors[2]}, #{colors[3][0]}: #{colors[3]}, #{colors[4][0]}: #{colors[4]}, #{colors[5][0]}: #{colors[5]}, #{colors[6][0]}: #{colors[6]}"
 end
